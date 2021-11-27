399e3f101f4d48705d8603bfb77251720ac6e35d
mdneyamul <neyamulsust@gmail.com>
2021-11-27 14:26:25 +0600

    improvement

--- university_management_system/main/urls.py
+++ university_management_system/main/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path ("register/",views.registerPage, name = 'register'),
-    path("login/",views.login, name= "login")
+    path("login/",views.loginPage, name= "login"),
+    path("logout/",views.logoutPage, name = "logout"),
+    path("",views.home, name= "home")
+
 
 
 ]
--- university_management_system/main/views.py
+++ university_management_system/main/views.py
@@ -10,6 +10,7 @@ from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import UpdateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 from .models import *
 from django.core import management
@@ -18,16 +19,44 @@ from django.core.management.commands import loaddata
 #from .EmailBackend import EmailBackend
 from .models import *
 from .forms import CreateUserForm
+from django.contrib import messages
 
-def login(request):
+from django.contrib.auth import authenticate, login, logout
+from .decorators import unauthenticated_user
+
+@unauthenticated_user
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request, "Username Or Password is not Correct")
+        
+
     return render(request, 'login_template/basic_elements.html')
+@unauthenticated_user
 def registerPage(request):
     form = CreateUserForm()
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account is created for ' + user)
             return redirect('login')
 
     context = {'form':form}
     return render(request,'registration_template/basic_elements.html',context)
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login') 
+@login_required(login_url = 'login')
+def home(request):
+    return render(request,'student_template/index.html')
